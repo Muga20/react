673a921602d1ea3a8dda31bde7d70608c7f58129
murimi <murimibrian@gmail.com>
2022-04-30 16:59:59 +0300

    commits

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+const navbar = () => {
+
+    return ( 
+    <div className="App">
+        <div className="navbar">
+            <div className="nav-tomorrow">
+                <ul>
+                    <li><h1 className="txt-tomorrowland">Tomorrowland</h1></li>
+
+                    <li><h1 className="txt-about">link</h1></li>
+                  
+                </ul>
+            </div>
+        </div>
+    </div>
+     );
+}
+ 
+export default navbar;
---
+++ src/components/greet.js
@@ -0,0 +1,8 @@
+// import React from "react";
+//  export const Greet=()=>{
+//      const title='Welcome to Tomorrow Land'
+//     return<div className="App">
+//         <h1>{title}</h1>
+//         </div>
+// }
+// export default Greet;
